45675df2eac40bc42c319452bc0065562644d6e2
Nathan Keller <keller.nathan@gmail.com>
2019-12-18 10:51:40 -0700

    Publication time frame pulls for all books but specific to the author.

--- lib/library.rb
+++ lib/library.rb
@@ -15,4 +15,25 @@ class Library
       author.books
     end
   end
+
+  def max_year
+    books.max_by do |book|
+      book.publication_year
+    end
+  end
+
+  def min_year
+    books.min_by do |book|
+      book.publication_year
+    end
+  end
+
+  def publication_time_frame_for(author)
+      @authors.reduce({}) do |acc, date|
+      acc[:start] = max_year.publication_year
+      acc[:end] = min_year.publication_year
+      acc
+    end
+    # time_frame = {start: 1, end: 1}
+  end
 end
--- test/library_test.rb
+++ test/library_test.rb
@@ -47,5 +47,22 @@ class LibraryTest < Minitest::Test
     assert_equal [jane_eyre, professor, villette, mockingbird], dpl.books
   end
 
+  def test_publication_time_frame
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+    expected_1 = {:start=>"1847", :end=>"1857"}
+    expected_2 = {:start=>"1960", :end=>"1960"}
+
+    assert_equal expected_1, dpl.publication_time_frame_for(charlotte_bronte)
+    assert_equal expected_2, dpl.publication_time_frame_for(harper_lee)
+  end
+
 
 end
